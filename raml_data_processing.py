8ef2059af1269bf065ec6741e11d9aa5bca1ee53
catherning <39702739+catherning@users.noreply.github.com>
2019-11-06 14:04:19 +0800

    Add main structure of IWSLT14 class that inherit SingleTurnDialog

--- raml_data_processing.py
+++ raml_data_processing.py
@@ -13,51 +13,125 @@ import os
 import sys
 import json
 
+from cotk._utils import hooks
+from cotk._utils.file_utils import get_resource_file_path
 from cotk.dataloader import SingleTurnDialog
 from utils import debug, Storage
 
-class OpenSubtitles(SingleTurnDialog):
-    	'''A dataloader for IWSLT14 dataset which is a Machine Learning translation dataset
-	Arguments:{ARGUMENTS}
-	Refer to :class:`.SingleTurnDialog` for attributes and methods.
-	References:
-		
-	'''
 
-	ARGUMENTS = SingleTurnDialog.ARGUMENTS
-	FILE_ID_DEFAULT = None
-	VALID_VOCAB_TIMES_DEFAULT = r'''Default: ``10``.'''
-	MAX_SENT_LENGTH = r'''Default: ``50``.'''
-	INVALID_VOCAB_TIMES_DEFAULT = r'''Default: ``0`` (No unknown words).'''
-	TOKENIZER_DEFAULT = r'''Default: ``nltk``'''
-	REMAINS_CAPITAL_DEFAULT = r'''Default: ``False``'''
-	@hooks.hook_dataloader
-	def __init__(self, file_id="resources://OpenSubtitles", min_vocab_times=10, \
-			max_sent_length=50, invalid_vocab_times=0, \
-			tokenizer="nltk", remains_capital=False\
-			):
-		super().__init__(file_id, min_vocab_times, max_sent_length, \
-			invalid_vocab_times, tokenizer, remains_capital)
+class IWSLT14(SingleTurnDialog):
+    '''A dataloader for IWSLT14 dataset which is a Machine Learning translation dataset
+    Arguments:{ARGUMENTS}
+    Refer to :class:`.SingleTurnDialog` for attributes and methods.
+    References:
+    '''
+
+    ARGUMENTS = SingleTurnDialog.ARGUMENTS
+
+    @hooks.hook_dataloader
+    def __init__(self, file_id, min_vocab_times=10,
+                 max_sent_length=50, invalid_vocab_times=0):
+        self._file_id = file_id
+        self._file_path = get_resource_file_path(file_id)
+        self._min_vocab_times = min_vocab_times
+        self._max_sent_length = max_sent_length
+        self._invalid_vocab_times = invalid_vocab_times
+        super(IWSLT14, self).__init__()
+
+    def _load_data(self):
+        r'''Loading dataset, invoked during the initialization of :class:`SingleTurnDialog`.
+        return vocab_list, valid_vocab_len, data, data_size
+        '''
+        
+        origin_data = {}
+        for key in self.key_name:
+            f_file = open("%s/opensub_pair_%s.post" % (self._file_path, key), 'r', encoding='utf-8')
+            g_file = open("%s/opensub_pair_%s.response" % (self._file_path, key), 'r', encoding='utf-8')
+            origin_data[key] = {}
+            origin_data[key]['post'] = list(map(lambda line: line.split(), f_file.readlines()))
+            origin_data[key]['resp'] = list(map(lambda line: line.split(), g_file.readlines()))
+
+        raw_vocab_list = list(chain(*(origin_data['train']['post'] + origin_data['train']['resp'])))
+        # Important: Sort the words preventing the index changes between different runs
+        vocab = sorted(Counter(raw_vocab_list).most_common(), key=lambda pair: (-pair[1], pair[0]))
+        left_vocab = list(filter(lambda x: x[1] >= self._min_vocab_times, vocab))
+        vocab_list = self.ext_vocab + list(map(lambda x: x[0], left_vocab))
+        valid_vocab_len = len(vocab_list)
+        valid_vocab_set = set(vocab_list)
+
+        for key in self.key_name:
+            if key == 'train':
+                continue
+            raw_vocab_list.extend(list(chain(*(origin_data[key]['post'] + origin_data[key]['resp']))))
+        vocab = sorted(Counter(raw_vocab_list).most_common(), \
+                       key=lambda pair: (-pair[1], pair[0]))
+        left_vocab = list( \
+            filter( \
+                lambda x: x[1] >= self._invalid_vocab_times and x[0] not in valid_vocab_set, \
+                vocab))
+        vocab_list.extend(list(map(lambda x: x[0], left_vocab)))
+
+        print("valid vocab list length = %d" % valid_vocab_len)
+        print("vocab list length = %d" % len(vocab_list))
+
+        word2id = {w: i for i, w in enumerate(vocab_list)}
+        line2id = lambda line: ([self.go_id] + \
+                    list(map(lambda word: word2id[word] if word in word2id else self.unk_id, line)) + \
+                    [self.eos_id])[:self._max_sent_length]
+
+        data = {}
+        data_size = {}
+        for key in self.key_name:
+            data[key] = {}
+
+            data[key]['post'] = list(map(line2id, origin_data[key]['post']))
+            data[key]['resp'] = list(map(line2id, origin_data[key]['resp']))
+            data_size[key] = len(data[key]['post'])
+            vocab = list(chain(*(origin_data[key]['post'] + origin_data[key]['resp'])))
+            vocab_num = len(vocab)
+            oov_num = len(list(filter(lambda word: word not in word2id, vocab)))
+            invalid_num = len( \
+                list( \
+                    filter( \
+                        lambda word: word not in valid_vocab_set, \
+                        vocab))) - oov_num
+            length = list(map(len, origin_data[key]['post'] + origin_data[key]['resp']))
+            cut_num = np.sum(np.maximum(np.array(length) - self._max_sent_length + 1, 0))
+            print("%s set. invalid rate: %f, unknown rate: %f, max length before cut: %d, \
+                    cut word rate: %f" % \
+                    (key, invalid_num / vocab_num, oov_num / vocab_num, max(length), cut_num / vocab_num))
+
+        return vocab_list, valid_vocab_len, data, data_size
+
+
+    def tokenize(self, sentence):
+        pass
+
+    def get_batch(self, key, indexes):
+        pass
+
 
 def main(args):
     data_class = SingleTurnDialog.load_class(args.dataset)
+    print(data_class)
     data_arg = Storage()
     data_arg.file_id = args.datapath
-    
+
     def load_dataset(data_arg):
         return data_class(**data_arg)
 
     return load_dataset(data_arg)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
 
     args = Storage()
 
-    args.dataset ="iwslt14"
+    args.dataset = "IWSLT14"
     args.datapath = "D:\\Documents\\THU\\Cotk\\data\\iwslt14"
 
-    # args.dataset ='OpenSubtitles'
-    # args.datapath ="D:/.cotk_cache/9cf4d4fbf4394c0725c4ad16bf60afd4a40e64c8465bde38d038586118a54888_unzip/opensubtitles/"
+    args.dataset ='OpenSubtitles'
+    args.datapath ="D:/.cotk_cache/9cf4d4fbf4394c0725c4ad16bf60afd4a40e64c8465bde38d038586118a54888_unzip/opensubtitles/"
 
     dm = main(args)
     dm.vocab_size
@@ -66,7 +140,7 @@ if __name__=="__main__":
     # all_vocab_list, all_vocab_size
     # batch_id {'dev': 0, 'test': 0, 'train': 0}, batchsize same with None values
     # data dict dev, test, train
-     # for dev: post : [[2,25,20,...],...], resp : [[2,95600,...],...]
+    # for dev: post : [[2,25,20,...],...], resp : [[2,95600,...],...]
     # datas_size for dev, test, trian
     # eos_id:3, go_id : 1, pad id 0, unk id 1
     # ext_vocab: tok above
